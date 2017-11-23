af49ca027f72335164ba7edc23c9092952778c3b
Alin <enachi.alin@gmail.com>
2017-11-24 01:38:32 +0200

    Sell button and update quantity
    Added logic to the Sell button that when it is pressed, it removes 1 product from the whole quantity of that specific product and updates the TextView

--- app/src/main/java/com/example/android/myinventory/Data/InventoryCursorAdapter.java
+++ app/src/main/java/com/example/android/myinventory/Data/InventoryCursorAdapter.java
@@ -1,7 +1,11 @@
 package com.example.android.myinventory.Data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -30,7 +34,7 @@ public class InventoryCursorAdapter extends CursorAdapter {
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, Cursor cursor) {
         //Fields to populate the inflated template
         TextView nameView = (TextView) view.findViewById(R.id.product_name);
         TextView quantityView = (TextView) view.findViewById(R.id.product_quantity);
@@ -42,17 +46,41 @@ public class InventoryCursorAdapter extends CursorAdapter {
         int priceColumnIndex = cursor.getColumnIndex(InventoryEntry.COLUMN_PRODUCT_PRICE);
         //read the product attributes from the cursor for the current product
         String productName = cursor.getString(nameColumnIndex);
-        String productQuantity = cursor.getString(quantityColumnIndex);
-        String productPrice = "$" + cursor.getString(priceColumnIndex);
+        final int quantity = cursor.getInt(quantityColumnIndex);
+        int price = cursor.getInt(priceColumnIndex);
+        //Set quantity and price as Strings so it can be assigned to the TextView
+        String productQuantity = String.valueOf(quantity);
+        String productPrice = "$" + String.valueOf(price);
         //populate the fields with the extracted proprieties
         nameView.setText(productName);
         quantityView.setText(productQuantity);
         priceView.setText(productPrice);
 
+        //Create the Uri for the product on the specific row
+        int id = cursor.getInt(cursor.getColumnIndex(InventoryEntry._ID));
+        final Uri mProductUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+
         sellButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(view.getContext(), "FUA", Toast.LENGTH_LONG).show();
+                ContentResolver resolver = view.getContext().getContentResolver();
+                ContentValues values = new ContentValues();
+                //Check if the quantity is greater than 0
+                //If so proceed by removing one at the time when pressing the sell button
+                if(quantity > 0){
+                    int qq = quantity;
+                    values.put(InventoryEntry.COLUMN_PRODUCT_QUANTITY, --qq);
+                    resolver.update(
+                           mProductUri,
+                            values,
+                            null,
+                            null
+                    );
+                    context.getContentResolver().notifyChange(mProductUri, null);
+                }else {
+                    //If the quantity is 0 - display a toast message
+                    Toast.makeText(view.getContext(), "You are out of stock", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
